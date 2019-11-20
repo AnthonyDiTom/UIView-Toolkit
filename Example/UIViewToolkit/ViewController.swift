d8a5505ebb3112aef92a4a5b45b3e5bbfafa56be
Anthony Di Tomasso <anthony.ditomasso@outlook.com>
2019-11-20 19:35:27 +0100

    add uiview+autolayout

--- Example/UIViewToolkit/ViewController.swift
+++ Example/UIViewToolkit/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import UIViewToolkit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        
+        [redView, blueView, greenView].forEach { view.addSubview($0) }
+        
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 12), size: .init(width: 125, height: 0))
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        
+        blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0))
+        blueView.anchorSize(to: redView)
+        
+        greenView.anchor(top: redView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 12, bottom: 0, right: 12))
+        
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        
+        greenView.addSubview(yellowView)
+        
+        yellowView.fillSuperview(padding: 20)
+        
+        let purpleView = UIView()
+        purpleView.backgroundColor = .purple
+        redView.addSubview(purpleView)
+        purpleView.fillSuperview(padding: .init(top: 5, left: 12, bottom: 5, right: 12))
+        
+        
+        let centeredView = UIView()
+        centeredView.backgroundColor = .brown
+        
+        yellowView.addSubview(centeredView)
+        
+        centeredView.centerInSuperview()
+        centeredView.anchorSize(width:50, height: 50)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
---
+++ UIViewToolkit/Classes/UIView+Autolayout.swift
@@ -0,0 +1,71 @@
+
+
+import UIKit
+
+extension UIView {
+    
+
+    public func fillSuperview(padding: UIEdgeInsets = .zero) {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor, padding: padding)
+    }
+    
+    public func fillSuperview(padding: CGFloat) {
+        
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor, padding: .init(top: padding, left: padding, bottom: padding, right: padding))
+    }
+    
+    public func centerInSuperview() {
+        
+        guard let superview = superview else {
+            print("Can't center \(self), because superview is nil")
+            return
+        }
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: superview.centerYAnchor).isActive = true
+        centerXAnchor.constraint(equalTo: superview.centerXAnchor).isActive = true
+    }
+    
+    public func anchorSize(width:CGFloat, height: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+
+    }
+    
+    public func anchorSize(to view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+    
+    public func anchor(top: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    
+    
+}
